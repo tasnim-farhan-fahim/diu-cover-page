44c6f415f9c874b547faf4c8c62193e8a2c6d761
Tasnim Farhan Fahim <fahimmd01927@gmail.com>
2024-07-13 01:16:30 +0600

    Project successfully finished

--- script.js
+++ script.js
@@ -8,60 +8,61 @@ document.querySelectorAll('.form').forEach(form => {
     const formId = event.target.id;
 
     if (formId==='assignment-form'){
-        documentContent += `<p ><strong><span class="assignment-text">ASSIGNMENT</span></strong></p>`;
+        documentContent += `<p class="page-header-text"><strong><span class="page-header-text-underline">ASSIGNMENT</span></strong></p>`;
 
-        documentContent += `<p ><strong> Course Code:  ${formData.get('courseCode')} </strong></p>`;
-        documentContent += `<p ><strong> Course Title:  ${formData.get('courseTitle')} </strong></p>`;
+        documentContent += `<p ><strong> Course Code: </strong> ${formData.get('courseCode')} </p>`;
+        documentContent += `<p ><strong> Course Title: </strong> ${formData.get('courseTitle')} </p>`;
 
-        documentContent += `<p ><strong> Topic Name:  ${formData.get('topicName')} </strong></p>`;
+        documentContent += `<p ><strong> Topic Name: </strong> ${formData.get('topicName')} </p>`;
 
         documentContent += `<p>   </p>`;
-        documentContent += `<p ><strong><span class="category-head-text-style">Submitted To: </span></strong></p>`;
-        documentContent += `<p class="space"><strong>Name:  ${formData.get('submittedToName')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Designation:  ${formData.get('submittedToDesignation')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Department:  ${formData.get('submittedToDepartment')} </strong></p>`;
+        documentContent += `<p class="category-head-text-style"><strong><span class="category-head-text-style-underline">Submitted To: </span></strong></p>`;
+        documentContent += `<p class="space"><strong>Name:  </strong>${formData.get('submittedToName')} </p>`;
+        documentContent += `<p class="space"><strong>Designation:  </strong>${formData.get('submittedToDesignation')} </p>`;
+        documentContent += `<p class="space"><strong>Department:  </strong>${formData.get('submittedToDepartment')} </p>`;
         documentContent += `<p class="space"><strong> Daffodil International University </strong></p>`;
         documentContent += `<p>   </p>`;
-        documentContent += `<p ><strong><span class="category-head-text-style">Submitted By: </span></strong></p>`;
-        documentContent += `<p class="space"><strong>Name:  ${formData.get('submittedByName')}  </strong></p>`;
-        documentContent += `<p class="space"><strong>ID:  ${formData.get('studentId')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Section:  ${formData.get('section')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Semester:  ${formData.get('semester')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Department:  ${formData.get('submittedByDepartment')} </strong></p>`;
-        documentContent += `<p class="space"><strong> Daffodil International University   </strong></p>`;
+        documentContent += `<p class="category-head-text-style"><strong><span class="category-head-text-style-underline">Submitted By: </strong></span></p>`;
+        documentContent += `<p class="space"><strong>Name:  </strong> ${formData.get('submittedByName')}  </p>`;
+        documentContent += `<p class="space"><strong>ID:  </strong> ${formData.get('studentId')} </p>`;
+        documentContent += `<p class="space"><strong>Section:  </strong> ${formData.get('section')} </p>`;
+        documentContent += `<p class="space"><strong>Semester:  </strong> ${formData.get('semester')} </p>`;
+        documentContent += `<p class="space"><strong>Department:  </strong> ${formData.get('submittedByDepartment')} </p>`;
+        documentContent += `<p class="space"><strong> Daffodil International University </strong></p>`;
         documentContent += `<p>   </p>`;
-        documentContent += `<p ><strong><span class="category-head-text-style">Submission Date:</span> ${formData.get('submissionDate')} </strong></p>`;
+        documentContent += `<p class="category-head-text-style"><strong><span class="category-head-text-style-underline">Submission Date:</strong></span> ${formData.get('submissionDate')} </p>`;
 
         document.getElementById('documentContent').innerHTML = documentContent;
         document.querySelector('.form').style.display = 'none';
         document.getElementById('document').style.display = 'block';
+
     }
     else if (formId==='lab-report-form'){
         
-        documentContent += `<p ><strong><span class="assignment-text">LAB REPORT</span></strong></p>`;
+        documentContent += `<p class="page-header-text"><strong><span class="page-header-text-underline">LAB REPORT</span></strong></p>`;
 
-        documentContent += `<p ><strong> Course Code:  ${formData.get('courseCode')} </strong></p>`;
-        documentContent += `<p ><strong> Course Title:  ${formData.get('courseTitle')} </strong></p>`;
+        documentContent += `<p ><strong> Course Code:  </strong>${formData.get('courseCode')} </p>`;
+        documentContent += `<p ><strong> Course Title:  </strong>${formData.get('courseTitle')} </p>`;
 
-        documentContent += `<p ><strong> Experiment No:  ${formData.get('experimentNo')} </strong></p>`;
-        documentContent += `<p ><strong> Experiment Name:  ${formData.get('experimentName')} </strong></p>`;
+        documentContent += `<p ><strong> Experiment No:  </strong>${formData.get('experimentNo')} </p>`;
+        documentContent += `<p ><strong> Experiment Name: </strong> ${formData.get('experimentName')} </p>`;
 
         documentContent += `<p>   </p>`;
-        documentContent += `<p ><strong><span class="category-head-text-style">Submitted To: </span></strong></p>`;
-        documentContent += `<p class="space"><strong>Name:  ${formData.get('submittedToName')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Designation:  ${formData.get('submittedToDesignation')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Department:  ${formData.get('submittedToDepartment')} </strong></p>`;
+        documentContent += `<p class="category-head-text-style"><strong><span class="category-head-text-style-underline">Submitted To: </span></strong></p>`;
+        documentContent += `<p class="space"><strong>Name:  </strong>${formData.get('submittedToName')} </p>`;
+        documentContent += `<p class="space"><strong>Designation:  </strong>${formData.get('submittedToDesignation')} </p>`;
+        documentContent += `<p class="space"><strong>Department:  </strong>${formData.get('submittedToDepartment')} </p>`;
         documentContent += `<p class="space"><strong> Daffodil International University </strong></p>`;
         documentContent += `<p>   </p>`;
-        documentContent += `<p ><strong><span class="category-head-text-style">Submitted By: </span></strong></p>`;
-        documentContent += `<p class="space"><strong>Name:  ${formData.get('submittedByName')}  </strong></p>`;
-        documentContent += `<p class="space"><strong>ID:  ${formData.get('studentId')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Section:  ${formData.get('section')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Semester:  ${formData.get('semester')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Department:  ${formData.get('submittedByDepartment')} </strong></p>`;
-        documentContent += `<p class="space"><strong> Daffodil International University   </strong></p>`;
+        documentContent += `<p class="category-head-text-style"><strong><span class="category-head-text-style-underline">Submitted By: </strong></span></p>`;
+        documentContent += `<p class="space"><strong>Name:  </strong> ${formData.get('submittedByName')}  </p>`;
+        documentContent += `<p class="space"><strong>ID:  </strong> ${formData.get('studentId')} </p>`;
+        documentContent += `<p class="space"><strong>Section:  </strong> ${formData.get('section')} </p>`;
+        documentContent += `<p class="space"><strong>Semester:  </strong> ${formData.get('semester')} </p>`;
+        documentContent += `<p class="space"><strong>Department:  </strong> ${formData.get('submittedByDepartment')} </p>`;
+        documentContent += `<p class="space"><strong> Daffodil International University </strong></p>`;
         documentContent += `<p>   </p>`;
-        documentContent += `<p ><strong><span class="category-head-text-style">Submission Date:</span> ${formData.get('submissionDate')} </strong></p>`;
+        documentContent += `<p class="category-head-text-style"><strong><span class="category-head-text-style-underline">Submission Date:</strong></span> ${formData.get('submissionDate')} </p>`;
 
         document.getElementById('documentContent').innerHTML = documentContent;
         document.querySelector('.form').style.display = 'none';
@@ -69,27 +70,27 @@ document.querySelectorAll('.form').forEach(form => {
     }
     else if (formId==='final-lab-report-form'){
         
-        documentContent += `<p ><strong><span class="assignment-text">FINAL LAB REPORT</span></strong></p>`;
+        documentContent += `<p class="page-header-text"><strong><span class="page-header-text-underline">FINAL LAB REPORT</span></strong></p>`;
 
-        documentContent += `<p ><strong> Course Code:  ${formData.get('courseCode')} </strong></p>`;
-        documentContent += `<p ><strong> Course Title:  ${formData.get('courseTitle')} </strong></p>`;
+        documentContent += `<p ><strong> Course Code:  </strong>${formData.get('courseCode')} </p>`;
+        documentContent += `<p ><strong> Course Title: </strong> ${formData.get('courseTitle')} </p>`;
 
         documentContent += `<p>   </p>`;
-        documentContent += `<p ><strong><span class="category-head-text-style">Submitted To: </span></strong></p>`;
-        documentContent += `<p class="space"><strong>Name:  ${formData.get('submittedToName')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Designation:  ${formData.get('submittedToDesignation')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Department:  ${formData.get('submittedToDepartment')} </strong></p>`;
+        documentContent += `<p class="category-head-text-style"><strong><span class="category-head-text-style-underline">Submitted To: </span></strong></p>`;
+        documentContent += `<p class="space"><strong>Name:  </strong>${formData.get('submittedToName')} </p>`;
+        documentContent += `<p class="space"><strong>Designation:  </strong>${formData.get('submittedToDesignation')} </p>`;
+        documentContent += `<p class="space"><strong>Department:  </strong>${formData.get('submittedToDepartment')} </p>`;
         documentContent += `<p class="space"><strong> Daffodil International University </strong></p>`;
         documentContent += `<p>   </p>`;
-        documentContent += `<p ><strong><span class="category-head-text-style">Submitted By: </span></strong></p>`;
-        documentContent += `<p class="space"><strong>Name:  ${formData.get('submittedByName')}  </strong></p>`;
-        documentContent += `<p class="space"><strong>ID:  ${formData.get('studentId')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Section:  ${formData.get('section')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Semester:  ${formData.get('semester')} </strong></p>`;
-        documentContent += `<p class="space"><strong>Department:  ${formData.get('submittedByDepartment')} </strong></p>`;
-        documentContent += `<p class="space"><strong> Daffodil International University   </strong></p>`;
+        documentContent += `<p class="category-head-text-style"><strong><span class="category-head-text-style-underline">Submitted By: </strong></span></p>`;
+        documentContent += `<p class="space"><strong>Name:  </strong> ${formData.get('submittedByName')}  </p>`;
+        documentContent += `<p class="space"><strong>ID:  </strong> ${formData.get('studentId')} </p>`;
+        documentContent += `<p class="space"><strong>Section:  </strong> ${formData.get('section')} </p>`;
+        documentContent += `<p class="space"><strong>Semester:  </strong> ${formData.get('semester')} </p>`;
+        documentContent += `<p class="space"><strong>Department:  </strong> ${formData.get('submittedByDepartment')} </p>`;
+        documentContent += `<p class="space"><strong> Daffodil International University </strong></p>`;
         documentContent += `<p>   </p>`;
-        documentContent += `<p ><strong><span class="category-head-text-style">Submission Date:</span> ${formData.get('submissionDate')} </strong></p>`;
+        documentContent += `<p class="category-head-text-style"><strong><span class="category-head-text-style-underline">Submission Date:</strong></span> ${formData.get('submissionDate')} </p>`;
 
         document.getElementById('documentContent').innerHTML = documentContent;
         document.querySelector('.form').style.display = 'none';
@@ -192,57 +193,47 @@ inputs.forEach((input, index) => {
 
 
 
-function hideButtons() {
-    document.querySelectorAll('button').forEach(button => {
-        button.style.display = 'none';
-    });
-}
-
-function showButtons() {
-    document.querySelectorAll('button').forEach(button => {
-        button.style.display = 'inline';
-    });
-}
-
 document.getElementById('downloadPdf').addEventListener('click', function() {
     const { jsPDF } = window.jspdf;
-    const doc = new jsPDF({
-        orientation: 'portrait',
-        unit: 'px',
-        format: [780, 1102.52],
-    });
 
-    hideButtons();  
+    html2canvas(document.getElementById('document') || document.getElementById('index-document'), {
+        scale: 2,  // Higher scale for better quality before compression
+        useCORS: true,
+        
+        backgroundColor: '#ffffff'
+    }).then(canvas => {
+        const imgData = canvas.toDataURL('image/jpeg', 0.75);  // Adjust quality parameter
 
-    doc.html(document.getElementById('document') || document.getElementById('index-document'), {
-        callback: function (doc) {
-            doc.save('cover-page.pdf');
-            showButtons();  // Show buttons after generating the PDF
-        },
-        html2canvas: {
-            scale: 0.986,  // Reduce scale to fit more content and reduce size
-            useCORS: true   // Use this option if you have cross-origin images
-        }
+        const doc = new jsPDF({
+            orientation: 'portrait',
+            unit: 'px',
+            format: [793, 1122.52],
+            compress: true  // Enable compression
+        });
+
+        const imgWidth = 793.7;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        
+        doc.addImage(imgData, 'JPEG', 0, 0, imgWidth, imgHeight);
+        doc.save('cover-page.pdf');
+
     });
 });
+
 document.getElementById('downloadImage').addEventListener('click', function() {
-    
     html2canvas(document.getElementById('document') || document.getElementById('index-document'), {
-        
-        scale: 4,  // Increase the scale for higher quality
+        scale: 4,  // Reduce the scale for better file size management
         useCORS: true,  // Use this option if you have cross-origin images
         backgroundColor: '#ffffff'
     }).then(canvas => {
         // Adjust the canvas dimensions for higher quality
         const imgWidth = 793.7;
-        const imgHeight = 1122.52;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
 
         const link = document.createElement('a');
-        link.href = canvas.toDataURL('image/jpeg', 0.7);  // Set the quality to 1.0
+        link.href = canvas.toDataURL('image/jpeg', 0.75);  // Set the quality to 0.75 for compression
         link.download = 'cover-page.jpeg';
         link.click();
     });
 });
-
-
 
